6a70130a130838907f647ff18710bbff31436482
macbook <macbook@MacBook-Pro-ASSO.local>
2025-09-09 23:23:53 +0000

    staeae

--- app/Http/Controllers/Api/PlanPaiementController.php
+++ app/Http/Controllers/Api/PlanPaiementController.php
@@ -21,7 +21,7 @@ class PlanPaiementController extends Controller
             $perPage = $request->input('per_page', 15);
             $search  = $request->input('search');
 
-            $query = PlanPaiement::with(['souscription']);
+            $query = PlanPaiement::with(['souscription.utilisateur']);
 
             if ($search) {
                 $query->where(function ($q) use ($search) {
